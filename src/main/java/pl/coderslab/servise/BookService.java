322f314bd0b0ac9aa0f4f686684c622e4c04e0fb
Maksim29AM <maks.abramovich.1995@mail.ru>
2025-06-29 09:39:09 +0200

    Dodano interfejs BookService z metodami CRUD

---
+++ src/main/java/pl/coderslab/servise/BookService.java
@@ -0,0 +1,21 @@
+package pl.coderslab.servise;
+
+import pl.coderslab.domain.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface BookService {
+    List<Book> getBooks();
+
+    void add(Book book);
+
+    Optional<Book> get(Long id);
+
+    void delete(Long id);
+
+    void update(Book book);
+
+}
+
